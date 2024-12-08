20e6518149a305ec8653a84ea032e1e3fdeb85c6
Massimiliano Demiri <150348788+massimiliano-demiri@users.noreply.github.com>
2024-12-08 15:22:05 +0100

    modifiche

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Abilita il supporto per la directory "app"
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
